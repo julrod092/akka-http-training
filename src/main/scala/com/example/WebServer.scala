174c10c0e37b2667e38b633b47d9a1c150dc3677
Julian A Rodriguez R <julianrodriguez@Julians-MacBook-Pro.local>
2017-03-12 19:48:22 -0500

    Created paths to GET, PUT, POST and DELETE

--- src/main/scala/com/example/WebServer.scala
+++ src/main/scala/com/example/WebServer.scala
@@ -2,31 +2,24 @@ package com.example
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
 import scala.io.StdIn
 
 object WebServer extends App {
 
-  implicit val system = ActorSystem("My-User-server")
+  // needed to run the route
+  implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
-  // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
-  val route =
-    path("hello") {
-      get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hello World!</h1>"))
-      }
-    }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val userRoute = new Routes
 
+  val bindingFuture = Http().bindAndHandle(userRoute.route, "localhost", 8080)
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
+    .onComplete(_ ⇒ system.terminate()) // and shutdown when done
 }
