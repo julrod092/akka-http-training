0d230c6e7d6501824994405f3f2de60e084feaab
Julian Rodriguez <julianrodriguez@seven4n.com>
2017-03-16 16:09:10 -0500

    Setting up cassandra in travis.yml

--- src/test/scala/com/example/users/services/UserServiceTest.scala
+++ src/test/scala/com/example/users/services/UserServiceTest.scala
@@ -1,13 +1,24 @@
 package com.example.users.services
 
+import java.util.UUID
+
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import com.example.infrastructure.database.ProductionDatabase
+import com.example.infrastructure.dto.UserDTO
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.concurrent.duration._
 
-class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest {
+
+class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest with ProductionDatabase{
 
   val userRoute = new UserRoute
+
+  override def beforeAll(): Unit = {
+    database.create(1.seconds)
+    database.userDAO.storeUser(UserDTO(UUID.fromString("5d50893c-0995-11e7-93ae-92361f002671"), "julrod092", "Julian", "Rodriguez"))
+  }
 
   "User service" should {
 
@@ -36,14 +47,11 @@ class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest {
       }
     }
 
-    "Create user and get it" in {
+    /*"Create user and get it" in {
       Post("/user") ~> userRoute.route ~> check {
 
       }
-      Get("/user/julrod092") ~> userRoute.route ~> check {
-        responseAs[String] shouldEqual """{"id":"5d50893c-0995-11e7-93ae-92361f002671","name":"julrod092","lastName":"Julian","userName":"Rodriguez"}"""
-      }
-    }
+    }*/
   }
 }
 
