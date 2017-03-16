595494809ce344ee44bf33eb3589e6eb0c764240
Julian Rodriguez <julianrodriguez@seven4n.com>
2017-03-16 16:32:40 -0500

    fail

--- src/test/scala/com/example/users/services/UserServiceTest.scala
+++ src/test/scala/com/example/users/services/UserServiceTest.scala
@@ -6,18 +6,22 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.example.infrastructure.database.ProductionDatabase
 import com.example.infrastructure.dto.UserDTO
+import com.example.users.persistence.UserRepository
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent.duration._
 
 
-class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest with ProductionDatabase{
+class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest with ProductionDatabase {
 
   val userRoute = new UserRoute
 
   override def beforeAll(): Unit = {
-    database.create(1.seconds)
-    database.userDAO.storeUser(UserDTO(UUID.fromString("5d50893c-0995-11e7-93ae-92361f002671"), "julrod092", "Julian", "Rodriguez"))
+    database.create(2.seconds)
+  }
+
+  override def afterAll(): Unit = {
+    database.drop(5.seconds)
   }
 
   "User service" should {
@@ -35,6 +39,12 @@ class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest wit
       }
     }
 
+    /*"Create user and get it" in {
+      Post("/user") ~> userRoute.route ~> check {
+
+      }
+    }
+
     "Verify if a user exist, if not, then return and empty string" in {
       Get("/user/julrod0921") ~> userRoute.route ~> check {
         responseAs[String] shouldEqual ""
@@ -44,12 +54,6 @@ class UserServiceTest extends WordSpec with Matchers with ScalatestRouteTest wit
     "Verify if a user exist, if exist, return the user data" in {
       Get("/user/julrod092") ~> userRoute.route ~> check {
         responseAs[String] shouldEqual """{"id":"5d50893c-0995-11e7-93ae-92361f002671","name":"julrod092","lastName":"Julian","userName":"Rodriguez"}"""
-      }
-    }
-
-    /*"Create user and get it" in {
-      Post("/user") ~> userRoute.route ~> check {
-
       }
     }*/
   }
