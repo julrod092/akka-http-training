79118904f24e257e378712080f87593a275a62d7
Julian Rodriguez <julianrodriguez@seven4n.com>
2017-03-22 16:15:20 -0500

    Testing with kafka

--- src/main/scala/com/example/infrastructure/broker/KafkaProducer.scala
+++ src/main/scala/com/example/infrastructure/broker/KafkaProducer.scala
@@ -1,25 +1,26 @@
 package com.example.infrastructure.broker
 
-import akka.http.scaladsl.model.StatusCodes
 import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.Producer
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import com.example.infrastructure.WebServer.system
 import com.example.infrastructure.dto.UserDTO
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContextExecutor
 
-object KafkaProducer {
+class KafkaProducer (implicit private val ec: ExecutionContextExecutor){
 
   private val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer).withBootstrapServers("localhost:9092")
+  implicit private val materializer = ActorMaterializer()
 
-  def sendMessage (userDTO: UserDTO): Future[(StatusCodes, String)] = {
+  def sendMessage (userDTO: UserDTO) : Unit = {
     Source.single(userDTO)
       .map { elem =>
         new ProducerRecord[Array[Byte], String]("test", s"User created: ${elem.userName}")
       }
-      .runWith(Producer.plainSink(producerSettings)).map(_ => (StatusCodes.OK, s"User created: ${userDTO.userName}"))
+      .runWith(Producer.plainSink(producerSettings))
   }
 }
--- src/main/scala/com/example/users/services/UserRoute.scala
+++ src/main/scala/com/example/users/services/UserRoute.scala
@@ -1,7 +1,6 @@
 package com.example.users.services
 
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.{Directives, Route}
 import com.example.infrastructure.dto.UserDTO
 import com.example.users.persistence.UserRepository
@@ -18,21 +17,21 @@ class UserRoute(userRepository: UserRepository)(implicit private val ec: Executi
         complete(userRepository.fetch(username))
       } ~
         delete {
-          complete(userRepository.delete(username).map(future => (OK, s"User deleted: $future"))
-            .recover { case e: Exception => (OK, e.toString) })
+          complete(userRepository.delete(username).map(future => (StatusCodes.OK, s"User deleted: $future"))
+            .recover { case e: Exception => (StatusCodes.InternalServerError, e) })
         }
     } ~
       path("user") {
         post {
           entity(as[UserDTO]) { user =>
-            complete(userRepository.add(user).map(future => )
-              .recover { case e: Exception => (OK, e.toString) })
+            complete(userRepository.add(user).map(future => (StatusCodes.OK, s"User created: ${future.userName}"))
+              .recover { case e: Exception => (StatusCodes.InternalServerError, e) })
           }
         } ~
           put {
             entity(as[UserDTO]) { user =>
-              complete(userRepository.update(user).map(future => (OK, s"User updated: ${future.userName}"))
-                .recover { case e: Exception => (OK, e.toString) })
+              complete(userRepository.update(user).map(future => (StatusCodes.OK, s"User updated: ${future.userName}"))
+                .recover { case e: Exception => (StatusCodes.InternalServerError, e) })
             }
           } ~
           get {
